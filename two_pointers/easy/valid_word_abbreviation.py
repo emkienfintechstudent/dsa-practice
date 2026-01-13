39a5456fa37c2b1f150c1354ebfe51af2f21ed30
Trần Trung Kiên <kientran.data@icebear.com.vn>
2026-01-13 20:56:25 +0700

    added 13-1-2026

--- two_pointers/easy/valid_word_abbreviation.py
+++ two_pointers/easy/valid_word_abbreviation.py
@@ -2,7 +2,7 @@ class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i = 0 
         j = 0
-        while i < len(word)-1 and j <len(abbr) -1:
+        while i <= len(word)-1 and j <=len(abbr)-1:
             print(word[i])
             print( word[j])
             if word[i] == abbr[j]:
@@ -17,10 +17,47 @@ class Solution:
                 while  ord('0') <= ord(abbr[j]) <= ord('9'):
                     num+= abbr[j]
                     j +=1
+                    if j > len(abbr) -1 : break
+                    
                 i += int(num)
-                if i>= len(word) or word[i] != abbr[j]: 
-                    return False 
+                print(i)
+                print(j)
+             
+                if i == len(word) and j ==len(abbr): 
+                        return True
+                elif i>= len(word): 
+                    return False
+                elif  word[i] != abbr[j]:
+                    return False
             else: 
                 return False
                 break
         return True
+    
+
+#optimize code
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i, j = 0, 0
+        m, n = len(word), len(abbr)
+        
+        while i < m and j < n:
+            if abbr[j].isdigit():
+                if abbr[j] == '0':  # Leading zeros are invalid
+                    return False
+                
+                num = 0
+                while j < n and abbr[j].isdigit():
+                    num = num * 10 + int(abbr[j])
+                    j += 1
+                
+                i += num
+                
+            elif word[i] == abbr[j]:
+                i += 1
+                j += 1
+                
+            else:
+                return False
+        
+        return i == m and j == n
